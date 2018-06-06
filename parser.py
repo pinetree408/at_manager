379bf107f531200c9f803842c0abe8a579a075e7
leesangyoon <pinetree408@gmail.com>
2018-06-06 14:38:42 +0900

    Fix reduction logic & Update overall logic with newly fixed reduction rule

--- parser.py
+++ parser.py
@@ -9,15 +9,12 @@ FOLDER_PATH = 'AT'
 ALL_NODE = {}
 NAME_EMPTY_NODES = {}
 MAX_CHILDREN = 0
-PREV_PRINTED_NODE_PARENT = -1
 
 def insert(tree, node):
     if len(tree['children']) == 0:
-        node['parent'] = tree['id']
         tree['children'].append(node)
     else:
         if tree['children'][-1]['depth'] == node['depth']:
-            node['parent'] = tree['id']
             tree['children'].append(node)
         elif tree['children'][-1]['depth'] < node['depth']:
             insert(tree['children'][-1], node)
@@ -31,15 +28,13 @@ def get_level(line):
             break
     return level
 
-def create_content_object(index, contents):
+def create_content_object(contents):
     matched_contents = re.findall('[a-zA-Z]+\s|[a-zA-Z]+=.[^=]+\s', contents + ' ')
     content_list = [content.strip() for content in matched_contents]
 
-    is_focusable = False
     html_tag = ""
     name = ""
 
-    var_object = {}
     for j, content in enumerate(content_list):
         if len(content.split('=')) == 2:
             target_key = content.split('=')[0]
@@ -52,14 +47,8 @@ def create_content_object(index, contents):
             elif target_key == "name":
                 if len(target_value) > 0:
                     name = target_value
-            else:
-                var_object[target_key] = target_value
-        else:
-            if content == "focusable":
-                is_focusable = True
 
     content_obj = {
-        'id': index,
         'atTag': content_list[0],
         'htmlTag': html_tag,
         'name': name,
@@ -75,17 +64,14 @@ def get_tree(f_name):
         lines = f_r.read().splitlines()
         for index, line in enumerate(lines):
             level = get_level(line)
-            item = create_content_object(index, line[level:])
+            item = create_content_object(line[level:])
             node = {
                 'id': index,
                 'depth': level/2,
-                'parent': -1,
                 'name': item['name'],
-                'nameList': [],
                 'atTag': item['atTag'],
                 'htmlTag': item['htmlTag'],
-                'reductedNode': [],
-                'children': []
+                'children': [],
             }
             if len(tree_data) == 0:
                 tree_data.append(node)
@@ -93,91 +79,54 @@ def get_tree(f_name):
                 insert(tree_data[0], node)
     return json.dumps(tree_data, indent=4)
 
-def get_content_list(f_name):
-    path = folder_name + '/' + f_name
-
-    node_list = []
-    with open(path, 'r') as f_r:
-        lines = f_r.read().splitlines()
-        for index, line in enumerate(lines):
-            level = get_level(line)
-            item = create_content_object(index, line[level:])
-            node_list.append(item)
-    return json.dumps(node_list, indent=4)
-
 def dfs_one_child_reduction(node):
     if len(node['children']) == 1:
-        reducted_node = {
-            'atTag': node['children'][0]['atTag'],
-            'htmlTag': node['children'][0]['htmlTag']
-        }
         p_name = node['name']
         c_name = node['children'][0]['name']
 
-        if p_name == '':
-            node.update(node['children'][0])
-        elif p_name != '' and c_name == '':
-            node['reductedNode'].append(reducted_node)
-            node['children'] = node['children'][0]['children']
-        elif p_name != '' and c_name !='':
-            if p_name == c_name:
-                node['reductedNode'].append(reducted_node)
-                node['children'] = node['children'][0]['children']
-            else:
-                print node['name'], node['atTag'], node['htmlTag']
-                node['nameList'].append(c_name)
-                node['reductedNode'].append(reducted_node)
-                node['children'] = node['children'][0]['children']
+        condition_one = (p_name == '') and (c_name == '')
+        condition_two = (p_name == '') and (c_name != '')
+        condition_three = (p_name != '') and (c_name == '')
+        condition_four = (p_name != '') and (c_name != '')
+        condition_four_one = condition_four and (p_name == c_name)
+        condition_four_two = condition_four and (p_name != c_name)
 
-        dfs_one_child_reduction(node)
+        if condition_one or condition_two or condition_three or condition_four_one:
+            new_name = p_name if len(p_name) >= len(c_name) else c_name
+            node['name'] = new_name
+            node['atTag'] = node['atTag'] + "-" + node['children'][0]['atTag']
+            node['htmlTag'] = node['htmlTag'] + "-" + node['children'][0]['htmlTag']
+            node['children'] = node['children'][0]['children']
+            dfs_one_child_reduction(node)
+        elif condition_four_two:
+            dfs_one_child_reduction(node['children'][0])
     else:
         for child in node['children'][:]:
             dfs_one_child_reduction(child)
 
-def dfs_update_parent_and_depth(node):
+def dfs_update_depth(node):
     for child in node['children'][:]:
-        child['parent'] = node['id']
         child['depth'] = node['depth'] + 1
-        dfs_update_parent_and_depth(child)
+        dfs_update_depth(child)
 
 def dfs_name_reduction_remain_parent(node):
     for child in node['children'][:]:
-        if len(child['name']) == 0:
-            if len(child['children']) == 0:
-                node['children'].remove(child)
-            else:
-                dfs_name_reduction_remain_parent(child)
+        if len(child['name']) == 0 and len(child['children']) == 0:
+            node['children'].remove(child)
         else:
             dfs_name_reduction_remain_parent(child)
 
-def dfs_name_reduction_remove_parent(node):
-    for child in node['children'][:]:
-        if len(child['name']) == 0:
-            if len(child['children']) == 0:
-                node['children'].remove(child)
-            else:
-                node['children'].extend(child['children'])
-                node['children'].remove(child)
-        else:
-            dfs_name_reduction_remove_parent(child)
-
 def get_tree_reduction(json_data):
     tree_data = json.loads(json_data)
-    prev_tree_data = copy.deepcopy(tree_data)
-    dfs_one_child_reduction(tree_data[0])
-    dfs_update_parent_and_depth(tree_data[0])
-    dfs_name_reduction_remain_parent(tree_data[0])
-    dfs_update_parent_and_depth(tree_data[0])
-    dfs_one_child_reduction(tree_data[0])
-    dfs_update_parent_and_depth(tree_data[0])
+    prev_tree_data = []
 
     while tree_data != prev_tree_data:
         print "reducting"
         prev_tree_data = copy.deepcopy(tree_data)
         dfs_name_reduction_remain_parent(tree_data[0])
-        dfs_update_parent_and_depth(tree_data[0])
         dfs_one_child_reduction(tree_data[0])
-        dfs_update_parent_and_depth(tree_data[0])
+
+    dfs_update_depth(tree_data[0])
     return json.dumps(tree_data, indent=4)
 
 def convert_json_to_txt(f_name, json_data):
@@ -185,8 +134,8 @@ def convert_json_to_txt(f_name, json_data):
         outfile.write(json_data)
 
 def dfs_analyze(node):
-    global ALL_NODE, NAME_EMPTY_NODES, MAX_CHILDREN, PREV_PRINTED_NODE_PARENT
-    html_tag = node["htmlTag"]
+    global ALL_NODE, NAME_EMPTY_NODES, MAX_CHILDREN
+    html_tag = node["htmlTag"].split('-')[0]
 
     if html_tag in ALL_NODE:
         ALL_NODE[html_tag] += 1
@@ -194,13 +143,6 @@ def dfs_analyze(node):
         ALL_NODE[html_tag] = 1
 
     if len(node["name"]) == 0:
-        '''
-        if html_tag == "div":
-            if PREV_PRINTED_NODE_PARENT != node["parent"]:
-                print "----"
-                print node
-                PREV_PRINTED_NODE_PARENT = node["parent"]
-        '''
         if html_tag in NAME_EMPTY_NODES:
             NAME_EMPTY_NODES[html_tag] += 1
         else:
